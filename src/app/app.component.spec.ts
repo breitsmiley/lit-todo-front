79e762e07e6eff63284280034613113a107e311e
Pavel Tsarenko <breitsmiley@gmail.com>
2019-05-29 21:20:20 +0300

    Updated Angular up to 8.0.0

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'lit-todo-front'`, () => {
+  it(`should have as title 'lit-todo-fron'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lit-todo-front');
+    expect(app.title).toEqual('lit-todo-fron');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lit-todo-front!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lit-todo-fron!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lit-todo-front';
+  title = 'lit-todo-fron';
 }
